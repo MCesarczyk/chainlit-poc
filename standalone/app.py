61ef2d27a437cee07397e77d51d7dc20c5ab5f98
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-11-17 21:27:59 +0100

    feat: use local model instead of external, enable auth

--- standalone/app.py
+++ standalone/app.py
@@ -1,29 +1,41 @@
-from openai import AsyncOpenAI
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 import chainlit as cl
-client = AsyncOpenAI()
+from typing import Optional
 
-# Instrument the OpenAI client
-cl.instrument_openai()
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
 
-settings = {
-    "model": "gpt-3.5-turbo",
-    "temperature": 1,
-    # ... more settings
-}
+ollama = OllamaLLM(
+    model="hf.co/SunJack/Qwen2-0.5b-finetuning:Q4_K_M",
+    base_url="http://ollama:11434"
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful bot, you always reply in Bulgarian"),
+    ("user", "{question}")
+])
+
+chain = prompt | ollama
+
+@cl.on_chat_start
+async def start():
+    cl.user_session.set("chain", chain)
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = await client.chat.completions.create(
-        messages=[
-            {
-                "content": "You are a helpful bot, you always reply in Bulgarian",
-                "role": "system"
-            },
-            {
-                "content": message.content,
-                "role": "user"
-            }
-        ],
-        **settings
-    )
-    await cl.Message(content=response.choices[0].message.content).send()
+    chain = cl.user_session.get("chain")
+    msg = cl.Message(content="")
+
+    async for chunk in chain.astream(
+        {"question": message.content},
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
